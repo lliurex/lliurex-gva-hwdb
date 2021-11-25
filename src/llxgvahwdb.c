2fa3f58ac38a94d530167b703f3e6f6e53410aba
Lt-Henry <quiqueiii@gmail.com>
2021-11-25 11:07:28 +0100

    working command line (in fact, feature complete)

--- src/llxgvahwdb.c
+++ src/llxgvahwdb.c
@@ -20,14 +20,69 @@
 #include "libllxgvahwdb.h"
 
 #include <stdio.h>
+#include <string.h>
+
+void show_help()
+{
+    fprintf(stderr,"LliureX GVA hardware database tool\n\n");
+    fprintf(stderr,"Available commands:\n");
+    fprintf(stderr,"what\t\tGuesses what gva model this system is\n");
+    fprintf(stderr,"is MODEL\tChecks if current system matches given MODEL name\n");
+    fprintf(stderr,"info\t\tDumps system info\n");
+}
 
 int main(int argc,char* argv[])
 {
+    if (argc<2) {
+        show_help();
+        return -1;
+    }
     
-    printf("%s\n",llx_gva_hwdb_get_vendor());
-    printf("%s\n",llx_gva_hwdb_get_system());
-    printf("%lu\n",llx_gva_hwdb_get_hash());
-    printf("What: %s\n",llx_gva_hwdb_what());
+    if (strcmp(argv[1],"info") == 0) {
+        printf("vendor: \"%s\"\n",llx_gva_hwdb_get_vendor());
+        printf("system: \"%s\"\n",llx_gva_hwdb_get_system());
+        const char* what = llx_gva_hwdb_what();
+        if (strlen(what) == 0) {
+            printf("what: \"UNKNOWN\"\n");
+        }
+        else {
+            printf("what: \"%s\"\n",what);
+        }
+        
+        return 0;
+    }
     
-    return 0;
+    if (strcmp(argv[1],"what") == 0) {
+        const char* what = llx_gva_hwdb_what();
+        if (strlen(what) == 0) {
+            printf("UNKNOWN\n");
+            return -2;
+        }
+        else {
+            printf("%s\n",what);
+            return 0;
+        }
+    }
+    
+    if (strcmp(argv[1],"is") == 0 && argc>2) {
+        const char* what = llx_gva_hwdb_what();
+        
+        if (strcmp(what,argv[2]) == 0) {
+            return 0;
+        }
+        else {
+            return -3;
+        }
+    }
+    
+    if (strcmp(argv[1],"compute-hash") == 0 && argc>2) {
+        uint64_t hash = llx_gva_hwdb_compute_hash(argv[2]);
+        printf("%ld\n",hash);
+        
+        return 0;
+    }
+    
+    show_help();
+    
+    return -1;
 }
